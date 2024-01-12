c0210754bd2ca018f222ba67e694b9b4fa8b183c
Oliver Rogoff <oliverogoff4@gmail.com>
2024-01-12 16:23:04 -0600

    Created parent bar class to inherit from

--- pyqt_custombar/parent_bar.py
+++ pyqt_custombar/parent_bar.py
@@ -1,36 +1,30 @@
-import math
 import sys
-
+import math
 from PyQt6.QtWidgets import QWidget
-from PyQt6.QtCore import Qt, QTimer, QRect
-from PyQt6.QtGui import QColor, QPainter, QPaintEvent
+from PyQt6.QtGui import QColor
 
 
-class ProgressBar(QWidget):
+class ParentBar(QWidget):
 
     def __init__(self,
                  parent: QWidget,
-                 determinate: bool = False,
-                 minimum: int  = None,
+                 minimum: int = None,
                  maximum: int = None,
                  center_on_parent: bool = True,
                  disable_parent_when_running: bool = False,
                  bar_length: int = None,
                  bar_height: int = None,
-                 min_opacity: float = 20,
-                 fade: float = 80.0,
-                 lines: int = 40,
-                 line_width: int = 2,
-                 roundness: float = 0,
                  color: tuple[int, int, int] = (0, 0, 0),
-
-                 segmented: bool = False,
                  ) -> None:
         super().__init__(parent)
 
-        self._is_determinate: bool = determinate
         self._center_on_parent: bool = center_on_parent
         self._disable_parent_when_running: bool = disable_parent_when_running
+
+        if minimum is not None and maximum is not None:
+            self._is_determinate = True
+        else:
+            self._is_determinate = False
 
         if self._is_determinate:
             if minimum is None or maximum is None:
@@ -39,91 +33,25 @@ class ProgressBar(QWidget):
             self._minimum = minimum
             self._maximum = maximum
 
-
         self._color: QColor = QColor(color[0], color[1], color[2])
-        self._minimum_trail_opacity: float = min_opacity
-        self._trail_fade_percentage: float = fade
-        self._number_of_lines: int = lines
-        # self._line_length: int = line_length
-        self._line_width: int = line_width
-        self._roundness: float = roundness
-        self._current_counter: int = 0
-
         self._bar_length = bar_length
         self._bar_height = bar_height
 
         self._current_value = 0
         self._percent_complete = 0.0
 
-        self._is_segmented = segmented
-
+        # self._current_counter: int = 0
         # self._timer: QTimer = QTimer(self)
         # self._timer.timeout.connect(self._rotate)
-        self._update_size()
-
-        if self._is_segmented:
-            self._number_of_lines = int(self.size().width() / (self._line_width + 2))
         # self._update_timer()
-        self.hide()
-
-        self._update_position()
         # self._is_spinning = True
-        self.show()
-
         # self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
-    def paintEvent(self, _: QPaintEvent) -> None:
-        """Paint the WaitingSpinner."""
-        self._update_position()
-        painter = QPainter(self)
-        painter.fillRect(self.rect(), Qt.GlobalColor.transparent)
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing, True)
-        if self._current_counter >= self._number_of_lines:
-            self._current_counter = 0
-        painter.setPen(Qt.PenStyle.NoPen)
-        relative_number_of_lines = int(self._percent_complete * self._number_of_lines)
-        for i in range(relative_number_of_lines):
-            painter.save()
-
-            # painter.translate(
-            #     self._inner_radius + self._line_length,
-            #     self._inner_radius + self._line_length,
-            # )
-            # rotate_angle = 360 * i / self._number_of_lines
-            # painter.rotate(rotate_angle)
-            # painter.translate(self._inner_radius, 0)
-            pos = int((self.size().width() * self._percent_complete) * i / relative_number_of_lines)
-            painter.translate(pos, 0)
-            painter.rotate(90)
-
-            distance = self._line_count_distance_from_primary(
-                i, self._current_counter, self._number_of_lines
-            )
-            color = self._current_line_color(
-                distance,
-                self._number_of_lines,
-                self._trail_fade_percentage,
-                self._minimum_trail_opacity,
-                self._color,
-            )
-            painter.setBrush(color)
-            painter.drawRoundedRect(
-                QRect(
-                    0,
-                    -self._line_width // 2,
-                    self._bar_height,
-                    self._line_width,
-                ),
-                self._roundness,
-                self._roundness,
-                Qt.SizeMode.RelativeSize,
-            )
-            painter.restore()
+        self._update_size()
+        self.hide()
 
     def _update_size(self) -> None:
         """Update the size of the ProgressBar."""
-        # size = (self._inner_radius + self._line_length) * 2
-        # self.setFixedSize(size, size)
         if self._bar_length is not None:
             self.setFixedWidth(self._bar_length)
 
@@ -190,6 +118,12 @@ class ProgressBar(QWidget):
         return self._percent_complete
 
     def set_value_by_percent(self, percent_complete: float):
+        if percent_complete > 1.0 or percent_complete < 0.0:
+            print("ERROR: PERCENT COMPLETE MUST BE FLOAT BETWEEN 0.0 AND 1.0")
+            if percent_complete > 1.0:
+                percent_complete = 1.0
+            else:
+                percent_complete = 0.0
         self._percent_complete = percent_complete
         self.repaint()
 
@@ -197,3 +131,7 @@ class ProgressBar(QWidget):
         self._current_value = value
         self._recalculate_percent_complete()
         self.repaint()
+
+    def start_bar(self):
+        self._update_position()
+        self.show()
